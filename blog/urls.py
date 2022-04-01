ba8bf6efd1f688f7506fbb8c205421204c88181a
martialmania <martialmania19@gmail.com>
2022-04-01 15:07:09 +0300

    Adding Comment to Admin

--- blog/urls.py
+++ blog/urls.py
@@ -12,18 +12,3 @@ urlpatterns = [
     path('user/<str:username>', UserPostListView.as_view(), name='user-post'),
     path('post/<int:pk>/comment/', views.add_comment, name='add_comment'),
 ]
-'''<a class = "btn btn-outline-info mb-4" href = "?page=1" > First < /a >
-<a class = "btn btn-outline-info mb-4" href = "?page = {{ page_obj.next_page_number }}" > Previous < /a >
-
-{% endif % }
-{ % for num in page_obj.paginator.page_range%}
-{% if page_obj.number == num % }
-<a class = "btn btn-info mb-4" href = "?page = {{ num }}" > {{num}} < /a >
-{%elif num > page_obj.number | add: '-3' and num %}
-<a class = "btn btn-outline-info mb-4" href = "?page = {{num }}" > {{num}} < /a >
-{% endif%}
-{% endfor % }
-{ % if page_obj.has_next%}
-<a class = "btn btn-outline-info mb-4" href = "?page={{page_obj.next_Page_number}}" > Next < /a >
-<a class = "btn btn-outline-info mb-4" href = "?page = {{ page_obj.paginator.num_pages }}" > Last <
-'''
